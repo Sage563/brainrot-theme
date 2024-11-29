2431d5b8cb3bd474ad52dbcb8bd5060eb8830144
Sage563 <gdsgps88@gmail.com>
2024-11-29 15:27:44 -0800

    Update gnome-install.sh

--- gnome-install.sh
+++ gnome-install.sh
@@ -1,4 +1,4 @@
 echo "Type 4 for the first prompt"
 printf "4\n" | sudo pacman -S  --noconfirm xorg xorg-server
-sudo pacman -S gdm
+sudo pacman -S  --noconfirm gdm
 printf "all"|sudo pacman -S --noconfirm gnome
